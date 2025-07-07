807bf0191465fec95281b608448b7c62936885d2
Gokulgurumurthy <gokulg4449@gmail.com>
2025-07-07 10:05:40 +0530

    JS syntax

---
+++ index.js
@@ -0,0 +1,26 @@
+// console.log("hello world");
+// console.log(`Hello world`);
+// console.log('Hello world');
+// console.log(100);
+// console.log(25+100);
+
+var store  = 100+100;
+var a = 1550+50;
+
+//console.log(a+a+store);
+// console.log(100 + 100 - 50 + 8 / 4 + 20 * 50);
+
+var store = 100 + 100; // 200
+var a = 100 + 300; // 400
+ 
+var b = store; // 200
+
+store = b + a; // 200 + 400
+store = store; // 600
+
+store = a - 400;
+a = store;
+b =  store + a;
+console.log(store, a, b);
+
+
