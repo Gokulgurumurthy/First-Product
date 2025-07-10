3d471cc6480670af7c62f5cf991c41dfba9c97ef
Gokulgurumurthy <gokulg4449@gmail.com>
2025-07-10 08:22:35 +0530

    new datatypes and variable

--- index.js
+++ index.js
@@ -1,26 +1,94 @@
-// console.log("hello world");
-// console.log(`Hello world`);
-// console.log('Hello world');
-// console.log(100);
-// console.log(25+100);
+// // console.log("hello world");
+// // console.log(`Hello world`);
+// // console.log('Hello world');
+// // console.log(100);
+// // console.log(25+100);
 
-var store  = 100+100;
-var a = 1550+50;
+// var store  = 100+100;
+// var a = 1550+50;
 
-//console.log(a+a+store);
-// console.log(100 + 100 - 50 + 8 / 4 + 20 * 50);
+// console.log(a+a+store);
+// // console.log(100 + 100 - 50 + 8 / 4 + 20 * 50);
 
-var store = 100 + 100; // 200
-var a = 100 + 300; // 400
+// var store = 100 + 100; // 200
+// var a = 100 + 300; // 400
  
-var b = store; // 200
+// var b = store; // 200
 
-store = b + a; // 200 + 400
-store = store; // 600
+// store = b + a; // 200 + 400
+// store = store; // 600
 
-store = a - 400;
-a = store;
-b =  store + a;
-console.log(store, a, b);
+// store = a - 400;
+// a = store;
+// b =  store + a;
+// console.log(store, a, b);
+
+// // variable declaration and initialization
+
+// var a ; // variable declaration
+
+// a = 190 // variable initialization
+
+// var b = 100 // variable initialization
+
+// var a = true; // boolean
+
+// a = 1000;
+
+// c =false;
+//  console.log(a,b,c);
+
+// types of storages or variables
+
+// var a = 10; // can be reintialize,redeclared,reassigned hoisting=undefined (consider hoisting)
+
+// let b = 10; // cannot  be reintialize,cannot be redeclared,can be reassigned ,hoisting=initialize error (doesnt entertain hoisting)
+
+// const c = 10; // cannot be reintialize,cannot be redeclared,cannot be reassigned, most used, hoisting=initialize error(doesnt entertain hoisting)
+
+// console.log(a,b,c);// hoisting  
+
+// var a = 10;
+
+// let b = 10;
+
+// datatypes
+
+// data types
+
+//  number, boolean, string, undefined, null
 
 
+// primitive datatypes
+
+// const a = undefined;
+// const b = false;
+// const c = 'string';
+// const d = 100;
+// const e = null
+
+// // non - primitive -> functions, object, arrays
+
+// function(){
+
+// }
+
+
+// const d = [];
+
+// const r = {};
+
+//Comparators,Logical operators and Strict Operators
+
+
+// const a = 11;
+
+// const b = 10
+
+// console.log(a > b)
+
+//Type coersion
+
+const a = 'hello';
+
+console.log(a+'world');
